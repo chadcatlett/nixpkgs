c77ac5c19485268c737b314af6ee259615149fd7
Chad <chad@catlett.info>
2023-06-06 13:35:03 -0500

    use newer zfs commit for unstable

--- pkgs/os-specific/linux/zfs/unstable.nix
+++ pkgs/os-specific/linux/zfs/unstable.nix
@@ -14,7 +14,7 @@ callPackage ./generic.nix args {
   #   zfs-2.1.9<=x<=2.1.10 is broken with aarch64-linux-6.2
   #   for future releases, please delete this condition.
   kernelCompatible = if stdenv'.isx86_64
-    then kernel.kernelOlder "6.3"
+    then kernel.kernelOlder "6.4"
     else kernel.kernelOlder "6.2";
   latestCompatibleLinuxPackages = linuxKernel.packages.linux_6_1;
 
@@ -22,8 +22,8 @@ callPackage ./generic.nix args {
   # IMPORTANT: Always use a tagged release candidate or commits from the
   # zfs-<version>-staging branch, because this is tested by the OpenZFS
   # maintainers.
-  version = "2.1.12-staging-2023-04-18";
-  rev = "e25f9131d679692704c11dc0c1df6d4585b70c35";
+  version = "2.1.12-staging-2023-06-06";
+  rev = "893549d6259a6904b7c1ee58080eb72acc4ff7aa";
 
   sha256 = "tJLwyqUj1l5F0WKZDeMGrEFa8fc/axKqm31xtN51a5M=";
 
