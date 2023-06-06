7de652cb405e082b450e9b1b08b4ea226dab1522
Chad <chad@catlett.info>
2023-06-06 14:18:49 -0500

    hacks

--- pkgs/os-specific/linux/zfs/unstable.nix
+++ pkgs/os-specific/linux/zfs/unstable.nix
@@ -25,7 +25,7 @@ callPackage ./generic.nix args {
   version = "2.1.12-staging-2023-06-06-my-hacks";
   rev = "5800f0d946615eb440ebf3d130beba9f356d7558";
 
-  sha256 = "tJLwyqUj1l5F0WKZDeMGrEFa8fc/=";
+  sha256 = "zzLwyqUj1l5F0WKZDeMGrEFa8fc/axKqm31xtN51a5M=";
 
   isUnstable = true;
   repoOwner = "chadcatlett";
